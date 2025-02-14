7ab7d4a738f0febd4eb3a3495464839f128cf158
rhflffkaksl <rhflffkaksl@gmail.com>
2025-02-15 01:58:14 +0900

    [D1] Title: 더블더블, Time: 93 ms, Memory: 26,752 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+			
+			int N=sc.nextInt();
+			int two=1;
+			System.out.print(1);
+			for(int i=1;i<=N;i++)
+			{
+				two*=2;
+				System.out.print(" "+two);
+
+			}
+			
+	
+}
+}
