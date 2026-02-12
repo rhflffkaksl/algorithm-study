f201e803252ead96bcc2223bb83a9dd0a2ee8eb8
rhflffkaksl <rhflffkaksl@gmail.com>
2026-02-12 23:47:19 +0900

    [Bronze II] Title: 시험 감독, Time: 380 ms, Memory: 121412 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int[] A = new int[N];
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      A[i] = Integer.parseInt(st.nextToken());
+    }
+
+    st = new StringTokenizer(br.readLine());
+
+    int B = Integer.parseInt(st.nextToken());
+    int C = Integer.parseInt(st.nextToken());
+
+    long cnt = 0;
+
+    for (int i = 0; i < N; i++) {
+      A[i] -= B;
+      cnt++;
+      if (A[i] > 0) {
+        cnt += A[i] / C + (A[i] % C != 0 ? 1 : 0);
+      }
+
+    }
+    System.out.println(cnt);
+  }
+}
